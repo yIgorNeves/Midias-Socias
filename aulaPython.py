825029301fa6ea9d4a4aec4f2b0995d19223433f
Igor Neves <yigorneves@gmail.com>
2019-09-10 16:33:56 -0300

    10/09/19 16:33

---
+++ aulaPython.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+autor = "Igor Neves"
+import sys
+
+def main():
+    print("Hello World!")
+    n=int(input())
+    for i in range(n):
+        print("Mensagem: %d"%i)
+    exit()
+
+if __name__ == '__main__':
+    main()
